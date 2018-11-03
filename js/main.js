f5bc2a859e2527dc13e867b81e47e08ceda1443d
Alexey Gerasimov <joahan.hax@gmail.com>
2018-11-04 05:48:43 +0800

    changing menu color on scroll added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).foundation();
 
 
+$(function() {
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 200 ) {
+          $("#desktopMenu").addClass("changeColor");
+       }
+       else {
+          $("#desktopMenu").removeClass("changeColor");
+       }
+    });
+ });
