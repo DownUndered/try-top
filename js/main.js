a8f6619dfaf3e9dc0750b660759253f695a76560
Alexey Gerasimov <joahan.hax@gmail.com>
2018-11-04 18:21:38 +0800

    desktop menu scroll animation adjust

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).foundation();
 
-
+var targetOffset = $("#shop").offset().top;
 $(function() {
     $(window).scroll(function () {
-       if ($(this).scrollTop() > 200 ) {
+       if ($(this).scrollTop() > targetOffset ) {
           $("#desktopMenu").addClass("changeColor");
        }
        else {
